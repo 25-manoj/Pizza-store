42209ae5d57badc6ec50651fa725e200144302d9
25-manoj <manojwebdevelop@gmail.com>
2024-04-14 15:35:33 +0530

    App layout implement

---
+++ src/ui/Header.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header>
+      <Link to="/">Pizza Store</Link>
+      <p>manoj</p>
+    </header>
+  );
+}
+
+export default Header;
