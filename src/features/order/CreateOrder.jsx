2e0ddfddc7162fcea01f1115484f2f03c427ea7e
25-manoj <manojwebdevelop@gmail.com>
2024-09-01 15:29:06 +0530

    styles implemented

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,5 +1,6 @@
 import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
+import Button from "../../ui/Button";
 
 // https://uibakery.io/regex-library/phone-number
 
@@ -43,57 +44,69 @@ function CreateOrder() {
   const cart = fakeCart;
 
   return (
-    <div>
-      <h2>Ready to order? Let's go!</h2>
+    <div className="px-4 py-6">
+      <h2 className="mb-8 text-xl font-semibold">Ready to order?</h2>
 
       <Form method="POST">
-        <div>
-          <label>First Name</label>
-          <input type="text" name="customer" required />
+        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+          <label className="sm:basis-40">First Name</label>
+          <input type="text" className="input grow" name="customer" required />
         </div>
 
-        <div>
-          <label>Phone number</label>
-          <div>
-            <input type="tel" name="phone" required />
-          </div>
-          {formErrors?.phone && <p>{formErrors.phone}</p>}
-        </div>
-
-        <div>
-          <label>Address</label>
-          <div>
-            <input type="text" name="address" required />
+        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+          <label className="sm:basis-40">Phone number</label>
+          <div className="grow">
+            <input className="input w-full" type="tel" name="phone" required />
+            {formErrors?.phone && (
+              <p className="mt-2 rounded-md bg-red-100 px-2 py-2 text-xs text-red-700">
+                {formErrors.phone}
+              </p>
+            )}
           </div>
         </div>
 
-        <div>
+        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+          <label className="sm:basis-40">Address</label>
+          <div className="grow">
+            <input
+              className="input w-full"
+              type="text"
+              name="address"
+              required
+            />
+          </div>
+        </div>
+
+        <div className="mb-12 flex items-center gap-5">
           <input
+            className="h-6 w-6 accent-yellow-400 focus:outline-none focus:ring
+            focus:ring-yellow-400 focus:ring-offset-2"
             type="checkbox"
             name="priority"
             id="priority"
             // value={withPriority}
             // onChange={(e) => setWithPriority(e.target.checked)}
           />
-          <label htmlFor="priority">Want to yo give your order priority?</label>
+          <label className="font-medium" htmlFor="priority">
+            Want to yo give your order priority?
+          </label>
         </div>
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <button
-            className="focus:ring-rellow-300 inline-block rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring
-            focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed"
-            disabled={isSubmitting}
-          >
+          <Button type="primary" disabled={isSubmitting}>
             {isSubmitting ? "Placing order..." : "Order now"}
-          </button>
+          </Button>
         </div>
       </Form>
     </div>
   );
 }
+
+// action //
+
 export async function action({ request }) {
-  const formData = await request.formData();
+  const formData = await request.formData(); // formData() is regular webAPI
   const data = Object.fromEntries(formData);
   const order = {
     ...data,
--- src/features/order/SearchOrder.jsx
+++ src/features/order/SearchOrder.jsx
@@ -4,15 +4,18 @@ import { useNavigate } from "react-router-dom";
 function SearchOrder() {
   const [query, setQuery] = useState("");
   const navigate = useNavigate();
+
   function handleSubmit(e) {
     e.preventDefault();
     if (!query) return;
     navigate(`/order/${query}`);
     setQuery("");
   }
+
   return (
     <form onSubmit={handleSubmit}>
       <input
+        className="rounded-full bg-yellow-100 px-4 py-2 text-sm placeholder:text-stone-400"
         placeholder="Search order #"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
